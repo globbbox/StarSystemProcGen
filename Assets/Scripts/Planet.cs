f2df7903dcefbf61efbdf781d4f9da97fe78509a
Denis Efimenko <gglobbox@mail.ru>
2024-06-02 23:20:54 +1000

    add sun

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Planet : MonoBehaviour
 {
     private float speed = 1.0f;
-	private float radius = 2f;
+	public float radius = 2f;
 	private float angle = 0f;
 	private float rotationSpeed = 100f;
 
+	public float xPos = 0f;
+	public float zPos = 0f;
 
 	void Start()
     {
@@ -22,7 +24,7 @@ public class Planet : MonoBehaviour
 
 		float x = Mathf.Cos(angle) * radius;
 		float z = Mathf.Sin(angle) * radius;
-		gameObject.transform.position = new Vector3(x, 0, z);
+		gameObject.transform.position = new Vector3(x + xPos, 0, z + zPos);
 		gameObject.transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
 
 
--- Assets/Scripts/StarSystemManager.cs
+++ Assets/Scripts/StarSystemManager.cs
@@ -1,23 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
+
+
+public class StarSystem
+{
+	public GameObject sun { get; set; }
+	public GameObject[] planets { get; set; }
+}
 
 public class StarSystemManager : MonoBehaviour
 {
+	
+
 	private float speed = 1.0f;
 	public float radius = 2f;
 	private float angle = 0f;
 	private float rotationSpeed = 100f;
+
+	private StarSystem system = null;
 	//private GameObject gameObject;
 
 
 	private void Awake()
 	{
-        var planetGen = new GanerateTexturePlanet(1000, 0.5f, 0.02f, 3.2f, 4);
+		system = new StarSystem();
+		system.sun = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		system.sun.transform.position = Vector3.zero;
+		system.sun.transform.localScale = new Vector3(5, 5, 5);
+		system.sun.GetComponent<Renderer>().material.color = Color.yellow;
+
+
+
+		var planetGen = new GanerateTexturePlanet(1000, 0.5f, 0.02f, 3.2f, 4);
+
         var a = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         a.transform.parent = transform;
-		//gameObject = a;
+		a.AddComponent<Planet>();
+		var comp = a.GetComponent<Planet>();
+		comp.xPos = system.sun.transform.position.x;
+		comp.zPos = system.sun.transform.position.z;
+		comp.radius = system.sun.transform.localScale.x + comp.transform.localScale.x / 2 + 2;
+		Debug.Log(comp.radius);
         planetGen.Apply(a);
+		
 	}
 
 	void Start()
@@ -26,15 +53,5 @@ public class StarSystemManager : MonoBehaviour
     }
 
 
-	//    void Update()
-	//    {
-	//		angle += speed * Time.deltaTime;
-
-	//		float x = Mathf.Cos(angle) * radius;
-	//		float z = Mathf.Sin(angle) * radius;
-	//		gameObject.transform.position = new Vector3(x, 0, z);
-	//		gameObject.transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
-
-
-	//	}
+	
 }
